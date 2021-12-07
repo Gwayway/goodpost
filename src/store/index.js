fbe74943308bf7f8863a4daaff0e6949206f4bd5
chuwill <952698119@qq.com>
2021-12-07 16:44:24 +0800

    setting 添加数据

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import home from './home';
+import setting from './setting'
 Vue.use(Vuex);
 export default new Vuex.Store({
     modules:{
         home,
+        setting
     }
 })
